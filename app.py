7e5bd99bafb5ecceaa546ee0096ee297228afbfb
ismet sarı <ismetsari@ismets-MacBook-Pro.local>
2020-11-07 22:40:50 +0300

    read lines

--- app.py
+++ app.py
@@ -1,41 +1,71 @@
 import threading
 import os
 import subprocess
+import json
+import sys
 
-"""
-def client():
+def my_server():
 
-    print("client dayım")
-    command = 'echo 123 | nc 192.168.1.38 12345 -c'
-    p = subprocess.Popen(
-    command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    print(p.communicate())
+
+
+
+    process = subprocess.Popen(
+    'nc -l -p 12345',
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    shell=True,
+    encoding='utf-8',
+    errors='replace'
+    )
+    index=0
+    while True:
+        index=index + 1
+        out = process.stdout.readline()
+        if out == '' and process.poll() != None:
+            break
+        if out != '':
+            
+            sys.stdout.write(out)
+            sys.stdout.flush()
+            if out =="}":
+                break
+        
     
 
-
-def my_server():
-    print("server dayım")
-    os.system("nc -l -p 12345 > chat.txt")
-    print("bitti mi=?")
-"""
-
-def my_server():
-
-    os.system("nc -l -p 12345 > log.txt")
-
-def client(message):
-
-    command = 'echo {} | nc 192.168.1.38 12345 -c'.format(message)
-
-    """p = subprocess.Popen(
-    command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    print(p.communicate())
-    """
+def client(message,to):
+    command = 'echo {} | nc {} 12345 -c'.format(message,to)
     subprocess.call(command, shell=True)
 
 
-server = threading.Thread(target=my_server)
-server.start()
-msg="merhaba"
-client = threading.Thread(target=client,args=("merhabasss",))
-client.start()
+def create_ip_list(local_ip,lan):
+    all_ips = {} 
+    for ipx in range(256): 
+        ip = lan+"."+str(ipx)
+        name = "user{}".format(str(ipx))
+        if ip != local_ip:
+            all_ips[ip] = name
+
+    json_object = json.dumps(all_ips, indent = 4)
+
+    with open("ips.json", "w") as outfile: 
+        outfile.write(json_object) 
+
+def get_local_ips():
+    local_ip = subprocess.getoutput("ipconfig getifaddr en0")
+    parse=local_ip.split(".")
+    lan=str(parse[0])+"."+str(parse[1])+"."+str(parse[2])
+    return (local_ip,lan)
+
+if __name__ == "__main__":
+    t=get_local_ips()
+    create_ip_list(local_ip=t[0],lan=t[1])
+    server = threading.Thread(target=my_server)
+    server.start()
+    data={"MY_IP":"192.168.1.3",
+          "NAME":"USER",
+          "TYPE":"DISCOVER",
+          "PAYLOAD":""}
+    msg=json.dumps(data)
+    client = threading.Thread(target=client,args=(msg,str(t[0])))
+    client.start()
+    

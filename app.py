222b7b88eeaa2a506727e27df1b36b3e42c8cc9c
İsmet Sarı <49066101+sarismet@users.noreply.github.com>
2020-11-08 23:42:05 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -23,14 +23,11 @@ def my_server(local_ip,response_json):
             errors='replace'
             )
             out = process.stdout.readline()
-            print("out is ",out,flush=True)
             start=out.find('{')
             end=out.find('}')+1
             msg=out[start:end]
-            files = open("l.txt","w")
-            files.write(out)
-            files.close()
             rejson = json.loads(msg)
+            print("the json loaded is :",rejson,flush=True)
             if rejson["TYPE"] == "DISCOVER":
                 response_msg = {
                     "MY_IP":"",
@@ -38,21 +35,45 @@ def my_server(local_ip,response_json):
                     "TYPE":"RESPOND",
                     "PAYLOAD":""
                 }
-                lan_turn=local_ip.split(".")[3]
-                username= "USER"+str(lan_turn)
+                is_writable = True
+                responses = response_json["response_array"]
+                for response in responses:
+                    if response["ip"] == rejson["MY_IP"]:
+                        is_writable = False
+                if is_writable:
+                    response_json["response_array"].append({"user_name":rejson["NAME"],"ip":rejson["MY_IP"]})
+                    with open("response_json.json", "w") as outfile: 
+                        outfile.write(json.dumps(response_json))
+
                 response_msg["MY_IP"]=local_ip
-                response_msg["NAME"] = username
-                
+                response_msg["NAME"] = "ismet"
                 client(json.dumps(response_msg),rejson["MY_IP"])
+                log_file = open("log.txt","a")
+                log_file.write("you got a DISCOVER message from "+rejson["MY_IP"]+" so send a RESPOND message \n")
+                log_file.close()
             elif rejson["TYPE"] == "RESPOND":
-                print()
-                response_json[rejson["MY_IP"]]=rejson["NAME"]
-                with open("response_json.json", "w") as outfile: 
-                    outfile.write(json.dumps(response_json))
+                
+                is_writable = True
+                responses = response_json["response_array"]
+                for response in responses:
+                    if response["ip"] == rejson["MY_IP"]:
+                        is_writable = False
+                if is_writable:
+                    response_json["response_array"].append({"user_name":rejson["NAME"],"ip":rejson["MY_IP"]})
+                    with open("response_json.json", "w") as outfile: 
+                        outfile.write(json.dumps(response_json))
+
+                log_file = open("log.txt","a")
+                log_file.write("you got a RESPOND message from "+rejson["MY_IP"]+"\n")
+                log_file.close()
             else:
+                log_file = open("log.txt","a")
+                log_file.write("you got a MESSAGE message from "+rejson["MY_IP"]+" which is "+rejson["PAYLOAD"]+"\n")
+                log_file.close()
                 os.system("clear")
                 print("msg is ",msg,flush=True)
                 
+            print("Whay do yo want to do \n Press m to send message\n Press d to send spesific discovery to an ip address\n",flush=True)
             sys.stdout.flush()
         except Exception as e:
             print("There is an error \n",e,flush=True)
@@ -91,43 +112,46 @@ def send_discovery(local_ip,lan):
     for last_number in range(0,256):
         temp_ip = lan+"."+str(last_number)
         if temp_ip != local_ip:
-            print("sending discovery to ",temp_ip)
-        msg = "\'"+json.dumps(data)+"\'"
-        try:
-            cmd = "echo '{}' | nc {} {}".format(msg,temp_ip,port)
-            subprocess.Popen(cmd, shell=True)
-        except:
-            pass
+            log_file = open("log.txt","a")
+            log_file.write("you send a DISCOVER message to "+temp_ip+"\n")
+            log_file.close()
+            msg = json.dumps(data)
+            try:
+                cmd = "echo '{}' | nc {} {}".format(msg,temp_ip,port)
+                subprocess.Popen(cmd, shell=True)
+            except:
+                pass
 
     sys.stdout.flush()
             
 def input_process(my_ip):
     os.system("clear")
     while(True):
-        request = input("Whay do yo want to do \n Press m to send message\n Press d to send spesific discovery to an ip address")
+        request = input("Whay do yo want to do \n Press m to send message\n Press d to send spesific discovery to an ip address\n")
         if request == "m":
-            print('Write you messsage',flush=True)
+            print('Write you messsage\n',flush=True)
             your_message=input()
             os.system("clear")
             json_data={"MY_IP":"",
-                        "NAME":"USER",
+                        "NAME":"ismet",
                         "TYPE":"MESSAGE",
                         "PAYLOAD":""}
             json_data["MY_IP"] = my_ip
             json_data["PAYLOAD"] = your_message
             msg=json.dumps(json_data)
-            print('Which ip do you want to send this message',flush=True)
-            ip_to_send=input()
-
-            """discovery_data={"MY_IP":"",
-                        "NAME":"USER",
-                        "TYPE":"DISCOVER",
-                        "PAYLOAD":""}
-            discovery_data["MY_IP"]=my_ip
-
-            client(json.dumps(discovery_data),ip_to_send)"""
-            print("msg is ",msg,flush=True)
+            print('Which user do you want to send this message\n',flush=True)
+            responses = response_json["response_array"]
+            index = 1
+            for response in responses:
+                print(index,".",response["user_name"],"\n",flush=True)
+                index = index + 1
+            user_order=int(input("give order\n"))
+            
+            ip_to_send = response_json["response_array"][user_order-1]["ip"]
             client(msg,ip_to_send)
+            log_file = open("log.txt","a")
+            log_file.write("you send a MESSAGE message to "+ip_to_send+"\n")
+            log_file.close()
         elif request == "d":
             print('Write the ip address',flush=True)
             ip_to_send=input()
@@ -138,8 +162,10 @@ def input_process(my_ip):
                         "PAYLOAD":""}
             json_data["MY_IP"] = my_ip
             msg=json.dumps(json_data)
-            print("msg is ",msg,flush=True)
             client(msg,ip_to_send)
+            log_file = open("log.txt","a")
+            log_file.write("you send a DISCOVER message to "+ip_to_send+"\n")
+            log_file.close()
         else:
             input_process(my_ip)
 
@@ -148,6 +174,7 @@ if __name__ == "__main__":
     with open("response_json.json", "r") as outfile: 
         response_json = json.load(outfile)
     
+
     hamachi = False
     try:
         if sys.argv[1] == "h":
@@ -155,13 +182,16 @@ if __name__ == "__main__":
     except:
         pass
     t=get_local_ips(hamachi)
-    """discovery_thread = threading.Thread(target=send_discovery,args=(t[0],t[1],))
-    discovery_thread.start()
-    discovery_thread.join()"""
     server = threading.Thread(target=my_server,args=(t[0],response_json,))
     server.start()
+    time.sleep(2)
+    discovery_thread = threading.Thread(target=send_discovery,args=(t[0],t[1],))
+    discovery_thread.start()
+    discovery_thread.join()
+    
 
     input_process(my_ip=t[0])
+
     
   
     
